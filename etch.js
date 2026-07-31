dd7415bb3ea2bd3dd1153d279c4a36f9695b0a95
Espeon-o-Megas <spchatzifotiadis@protonmail.com>
2026-07-31 12:27:23 +0300

    Made the drawing pad and number of tiles flex

--- etch.js
+++ etch.js
@@ -1,20 +1,32 @@
+/*
+I need to make a change so that:
+-The drawing pad is flexible.
+-The number of tiles changes depending on the pads shape and size.
+My idea:
+-grid size is number of horizontal tiles.
+-the size of these tiles is grid-width/grid-size
+-since they are square, the number of tile rows is grid-height/tile-size
+-the total number of squares needed is the horizontal amount times the vertical, 
+ which is: 
+*/
+
 let gamebox =document.querySelector('.gamebox');
 
-let drawingPad = document.createElement('div');
-drawingPad.classList.add('drawPad');
-gamebox.appendChild(drawingPad);
+let drawingPad = document.querySelector('.drawPad');
 
-let gridSize=30;
-let width = Math.floor(700/gridSize);
-console.log(width);
+let tilesPerRow=25;
+let tileSize= Math.floor((drawingPad.offsetWidth-20)/tilesPerRow);
+console.log(tileSize);
+let tileRows= Math.floor((drawingPad.offsetHeight-20)/tileSize);
+console.log(tileRows);
 fillPad();
 
 function fillPad () {
- for (let i=0; i<gridSize*gridSize; i++) {
+ for (let i=0; i<tilesPerRow*tileRows; i++) {
   let tile = document.createElement('div');
   tile.classList.add('tile');
-  tile.style.width=(width+'px');
-  tile.style.height=(width+'px');
+  tile.style.width=(tileSize+'px');
+  tile.style.height=(tileSize+'px');
   tile.addEventListener('mousemove',(event)=>{
    if (event.buttons===1 && event.ctrlKey===false){
     event.target.classList.add('coloredTile');
