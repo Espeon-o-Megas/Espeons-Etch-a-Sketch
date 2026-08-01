1a9ee119b111ae09bb4e68c2bb6e8c85df2f5f4a
Espeon-o-Megas <spchatzifotiadis@protonmail.com>
2026-08-01 13:14:41 +0300

    Minor tweaks.
    I was working on a non-desctuctive resizing script, remnants of which can still be seen in the code.
    I'll tackle it again soon.

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # Espeons-Etch-a-Sketch
 This is another fun task for the Odin Project. 
-In it's simplest form, this site creates a square grid of tiles that you can paint using your mouse. 
+In it's simplest form, this site creates a square grid of tiles that you can paint using your mouse (or finger!)
+I've added a simple grid-size manipulation system plus an eraser for mouse users (hold ctrl!).
 
-I've tweaked the base recipe a bit, making the drawing area flexible depending on your viewing window and adding an eraser feature to clean up any mistakes.
-
-I'm planning to add progressive darkening of tiles, color selection and the ability to tweak the canvas size from inside the page.
+I'm planning to add progressive darkening of tiles, color selection and a proper toolbar!
--- etch.js
+++ etch.js
@@ -1,15 +1,15 @@
 const drawingPad = document.querySelector('.drawPad');
-let gridSize=30;
+let gridSize=32;
 updateGridSizeDisplay();
 padFill ();
 
 function updateGridSizeDisplay () {
  const midB = document.querySelector('#currentGridSize');
- midB.textContent=gridSize;
+ midB.textContent=(gridSize +' X '+ gridSize);
 }
 
 function padFill (){
- let padWidth = drawingPad.offsetWidth-20
+ let padWidth = drawingPad.offsetWidth-40
  let tileSize = Math.floor(padWidth/gridSize);
  drawingPad.style.height=(padWidth+'px');
  for (let i=0; i<gridSize*gridSize; i++) {
@@ -42,8 +42,8 @@ function padClear () {
 let rightButton = document.querySelector('.plus');
 rightButton.addEventListener('click', ()=>{
 padClear();
-if (gridSize<=62){
- gridSize +=2;
+if (gridSize<=60){
+ gridSize +=4;
 } else {
  gridSize = 64;
 }
@@ -54,60 +54,11 @@ padFill();
 let leftButton = document.querySelector('.minus');
 leftButton.addEventListener('click', ()=>{
 padClear();
-if (gridSize>=10){
- gridSize -=2;
+if (gridSize>=12){
+ gridSize -=4;
 } else {
  gridSize = 8;
 }
 updateGridSizeDisplay();
 padFill();
 })
-
-
-
-/* const gamebox =document.querySelector('.gamebox');
-const drawingPad = document.querySelector('.drawPad');
-const displayB = document.querySelector('#currentTilesPerRow')
-let tilesPerRow=45;
-displayB.textContent=tilesPerRow;
-fillPad();
-
-function fillPad () {
- let tileSize= Math.floor(((drawingPad.offsetWidth-20)/tilesPerRow)-2);
- let tileRows= Math.floor(((drawingPad.offsetHeight-20)/tileSize)-2);
- 
- for (let i=0; i<tileRows; i++) {
- for (let j=0; j<tilesPerRow; j++){
-  let tile = document.createElement('div');
-  tile.classList.add('tile');
-  tile.style.width=(tileSize+'px');
-  tile.style.height=(tileSize+'px');
-  tile.addEventListener('mousemove',(event)=>{
-   if (event.buttons===1 && event.ctrlKey===false){
-    event.target.classList.add('coloredTile');
-   } else if (event.buttons===1 && event.ctrlKey===true){
-    event.target.classList.remove('coloredTile');
-   }
-  })
-  tile.addEventListener('touchmove',(event)=>{
-    event.target.classList.add('coloredTile');
-  })
-  drawingPad.appendChild(tile);
- }}
-}
-
-function clearPad() {
- let tileSize= Math.floor(((drawingPad.offsetWidth-20)/tilesPerRow)-2);
- let tileRows= Math.floor(((drawingPad.offsetHeight-20)/tileSize)-2);
- for (let i=0; i<tileRows; i++) {
- for (let j=0; j<tilesPerRow; j++){
-  drawingPad.removeChild(drawingPad.firstChild);
- }}
-}
-
-let lb = document.querySelector('#reduceGridSize')
-lb.addEventListener('click', ()=>{
- clearPad();
- tilesPerRow=tilesPerRow-5;
- fillPad();
-})  drawingPad.removeChild(drawingPad.firstChild); */
